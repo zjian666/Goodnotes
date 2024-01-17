28b3e3c3771bc8af92cca3995b8f8682ce232794
zjian666 <88768780+zjian666@users.noreply.github.com>
2024-01-17 10:19:26 +0800

    Create Goodnotes6.js

---
+++ Goodnotes6.js
@@ -0,0 +1,13 @@
+#!name = GG6
+
+[MITM]
+hostname = %APPEND% api.revenuecat.com, isi.csan.goodnotes.com
+[Header Rewrite]
+#Anticache
+.+ header-del if-modified-since
+.+ header-del if-none-match
+.+ header-del X-RevenueCat-ETag
+.+ header-del x-revenuecat-etag
+
+[Rewrite_Local]
+^https:\/\/(api\.revenuecat|isi.csan.goodnotes)\.com\/.+\/(receipts$|subscribers\/[^/]+$|offers$) https://api.langkhach89.workers.dev header
