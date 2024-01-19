717c8b9d10abea11b03d3027ca18102cc0d3ed0b
zjian666 <88768780+zjian666@users.noreply.github.com>
2024-01-19 11:30:35 +0800

    Update Zjian's Rewrite.js

--- Zjian's Rewrite.js
+++ Zjian's Rewrite.js
@@ -1,22 +1,22 @@
 # 彩云天气
-hostname = *.cyapi.cn, *.caiyunapp.com, adx.sogaha.cn
+# hostname = *.cyapi.cn, *.caiyunapp.com, adx.sogaha.cn
 ^https?:\/\/(biz|wrapper|starplucker)\.(cyapi|caiyunapp)\.(cn|com)\/(.+\/(user\?app_name|activity\?app_name|visitors|operation\/banners)|p\/v\d\/(vip_info|user_info)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
 ^https?:\/\/(api|wrapper)\.(cyapi|caiyunapp)\.(cn|com)\/v\d\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
 
 # 百度网盘，一刻相册 解锁部分功能
-hostname = pan.baidu.com
+# hostname = pan.baidu.com
 ^https?:\/\/pan\.baidu\.com\/(youai\/(user\/.+\/getminfo|membership\/.+\/adswitch)|(rest\/.+\/membership\/user|act\/.+\/(bchannel|welfare)\/list|api\/usercfg)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/bdcloud.js
 
 # 阿里云盘 净化/解锁SVIP
-hostname = *.aliyundrive.com, *.alipan.com
+# hostname = *.aliyundrive.com, *.alipan.com
 ^https?:\/\/(api|member)\.(aliyundrive|alipan)\.com\/(.+\/(users|activity|user\/get)|((business|apps)\/.+\/users|adrive\/.+\/user)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/alidrive.js
 
 # 美图秀秀
-hostname = *.xiuxiu.meitu.com, api.posters.meitu.com, api-*.meitu.com
+# hostname = *.xiuxiu.meitu.com, api.posters.meitu.com, api-*.meitu.com
 ^https?:\/\/((h5|api)\.xiuxiu|api-sub|api\.posters)\.meitu\.com\/.+\/(vip|user|h\d|center|home) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/mtxx.js
 
 # Fimo_复古胶片相机
-hostname = server.*.com
+# hostname = server.*.com
 ^https?:\/\/server\.(yoyiapp|zbisq)\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/fimo.js
 
 # Documents
@@ -52,4 +52,7 @@ hostname = dict.youdao.com, business.youdao.com, api-overmind.youdao.com, cdke.y
 ^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/SexActions.js
 hostname = api.revenuecat.com
 
+
+
+hostname = *.cyapi.cn, *.caiyunapp.com, adx.sogaha.cn, pan.baidu.com, *.aliyundrive.com, *.alipan.com, *.xiuxiu.meitu.com, api.posters.meitu.com, api-*.meitu.com, server.*.com
 
